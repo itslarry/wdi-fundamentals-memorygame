1c5361695a735442ac369547efd45536e47c3953
Larry <lawrencema415@gmail.com>
2019-05-15 19:41:39 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,11 +1,34 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+  {
+    rank:"queen",
+    suit:"hearts",
+    cardImage:"images/queen-of-hearts.png"
+  },
+  {
+    rank:"queen",
+    suit:"diamonds",
+    cardImage:"images/queen-of-hearts.png"
+  },
+  {
+    rank:"king",
+    suit:"hearts",
+    cardImage:"images/king-of-hearts.png"
+
+  },
+  {
+    rank:"king",
+    suit:"diamonds",
+    cardImage:"images/king-of-hearts.png"
+
+  }
+]
 var cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
   } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
   }
 
 }
@@ -13,8 +36,10 @@ function checkForMatch() {
 
 
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].suit);
+  console.log("User flipped " + cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
 }
 flipCard(0);
 flipCard(2);
