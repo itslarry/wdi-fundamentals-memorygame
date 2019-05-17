0b22c985b6a0b31f0a8b3ac72bfa2bd987bb66e6
Larry <lawrencema415@gmail.com>
2019-05-17 11:59:39 -0700

    added score and reset button

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -13,40 +13,50 @@ var cards = [
     rank:"king",
     suit:"hearts",
     cardImage:"images/king-of-hearts.png"
-
   },
   {
     rank:"king",
     suit:"diamonds",
     cardImage:"images/king-of-hearts.png"
-
   }
 ]
 
 var cardsInPlay = [];
 
+var currentScore = 0;
+
+
 function flipCard() {
   var CardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[CardId]);
   this.setAttribute('src',cards[this.getAttribute('data-id')].cardImage);
-
-  if (cardsInPlay.length === 2) {
-    checkForMatch();
-  }
 }
 
 
 function checkForMatch() {
-
+  console.log(cardsInPlay[0]);
+  console.log(cardsInPlay[1]);
+  if (cardsInPlay.length === 2 || cardsInPlay.length === 4) {
     if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
       console.log("You found a match!");
+      currentScore += 50;
+      document.getElementById('score').innerHTML = currentScore;
+      if(currentScore == 100) {
+        alert("Congradulation, you won!");
+      }
     } else {
       alert("Sorry, try again.");
     }
-
-
-
+  }
 }
+
+function reset() {
+  document.location.reload();
+  console.log("reset!");
+}
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click',reset);
 
 createBoard = function() {
 for(var i = 0;i < cards.length;i++) {
@@ -56,9 +66,7 @@ for(var i = 0;i < cards.length;i++) {
     cardElement.addEventListener('click',flipCard);
     cardElement.addEventListener('click',checkForMatch);
     document.getElementById('game-board').appendChild(cardElement);
-
   }
 }
-
 
 createBoard();
